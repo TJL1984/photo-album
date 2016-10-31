e38a8f02b9efe5161d18a32d967177557ec72a16
TJ Liwanag <timothyjliwanag@gmail.com>
2016-10-31 15:22:42 -0400

    targeted the images so that when the user clicks on one, the image zooms in

--- js/photo-album.js
+++ js/photo-album.js
@@ -1,7 +1,16 @@
 (function() {
   'use strict';
-  
+
+console.log( $('main') )
+
+$('.photos')
+  .find('li')
+    .on('click', function zoom(event) {
+      // console.log('click');
+      $(this).toggleClass('zoomed')
+      $('main').toggleClass('zooming')
+
+    })
 
 
-
-})
+})();
