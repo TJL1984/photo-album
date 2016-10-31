735a6d4339300bd79bc680ed3d9f9c60e98a21d7
TJ Liwanag <timothyjliwanag@gmail.com>
2016-10-31 17:18:43 -0400

    I added functions to the left/right buttons, but after 1 use, the buttons disappear

--- js/photo-album.js
+++ js/photo-album.js
@@ -11,10 +11,31 @@ $('.photos')
       $(this).toggleClass('zoomed')
       $('main').toggleClass('zooming')
       $(this).append($ ('nav') );
-    })
+    });
 
 
+// I need to have the left button change to
+// the picture on the left of current image.
+$('.previous')
+    .on('click', function moveLeft() {
+      // console.log('click');
 
+      $(this).parent().parent().previous('li')
+      .toggleClass ('zoomed')
+      .append($ ('nav') )
+
+    });
+
+
+$('.next')
+      .on('click', function moveRight() {
+      // console.log('click');
+
+      $(this).parent().parent().next('li')
+      .toggleClass ('zoomed')
+      .append($ ('nav') )
+
+    });
 
 
 
