51e91fc4e3ec7fe0e2598f91d45ae2d6cd1b6a12
TJ Liwanag <timothyjliwanag@gmail.com>
2016-10-31 15:51:46 -0400

    need to apply a nav element

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # photo-album
+
+I am creating a responsive photo album app.
+
+When user clicks an image, the image zooms.
+
+I need to be able to add a nav inserted at the end of the image whenever its zoomed.
--- js/photo-album.js
+++ js/photo-album.js
@@ -4,13 +4,14 @@
 console.log( $('main') )
 
 $('.photos')
-  .find('li')
-    .on('click', function zoom(event) {
-      // console.log('click');
+    .find('li')
+    .on('click', function zoom() {
+      console.log('click');
       $(this).toggleClass('zoomed')
       $('main').toggleClass('zooming')
-
     })
+
+$('.')
 
 
 })();
